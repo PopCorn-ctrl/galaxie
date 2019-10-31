3ae05447c143106e2bca51de908a79f6914a711f
PopCorn-ctrl <56980133+PopCorn-ctrl@users.noreply.github.com>
2019-10-31 17:57:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ client.on('message', function (message) {
  
     if (args[0].toLowerCase() === prefix + 'halloween') {
        if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'utiliser cette commande ;(")
-       message.channel.send("**Joyeux** __***Halloween***__ 🎃🎉! \n __Tout__ **le staff de souhaite un très beux Halloween !** n\ __**Le Staff**__")        
+       message.channel.send("**Joyeux** __***Halloween***__ 🎃🎉! __Tout__ **le staff de souhaite un très beux Halloween !** n\ __**Le Staff**__")        
 
     }
 })
